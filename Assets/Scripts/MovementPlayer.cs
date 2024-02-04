8f7cd3c28512099b52bb07685005a97f7e6368de
TPGGschool <tportales@cocoschool.mx>
2024-02-03 22:54:57 -0600

    Sprites Agregados
    agregue unos cuantos sprites y metí animacion idle y de caminar

--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class MovementPlayer : MonoBehaviour
 {
     // variables generales del script
-    private float MoveSpeed;
+    private float MoveSpeed = 700;
     [SerializeField]
     private SpriteRenderer Sprite;
     private Rigidbody2D rbPlayer;
-    private float JumpForce = 5;
-    private bool InFloor;
+    private float JumpForce = 6;
     int Jumps = 2;
+    private Animator animator;
 
     void Start()
     {
@@ -20,6 +20,7 @@ public class MovementPlayer : MonoBehaviour
 
         rbPlayer = gameObject.GetComponent<Rigidbody2D>();
 
+        animator = GetComponent<Animator>();
 
     }
 
@@ -41,22 +42,32 @@ public class MovementPlayer : MonoBehaviour
         // sprint
         if (Input.GetKey(KeyCode.LeftShift))
         {
-            MoveSpeed = 45000;
+            MoveSpeed = 1400;
+            animator.SetBool("IsRuning", true);
         }
-        else MoveSpeed = 22500;
+        else
+        {
+            MoveSpeed = 700;
+            animator.SetBool("IsRuning", false);
+        }
 
         // voltear sprite
         if (Move.x < 0)
         {
-            Sprite.flipX = true;
-        }
-        if (Move.x > 0)
-        {
             Sprite.flipX = false;
+            animator.SetBool("Iswalking", true);
+
         }
+        else if (Move.x > 0)
+        {
+            Sprite.flipX = true;
+            animator.SetBool("Iswalking", true);
+        }
+        else animator.SetBool("Iswalking", false);
     }
     private void Jump()
     {
+        // salto
         if (Input.GetKeyDown(KeyCode.Space) && Jumps < 2) 
         {
             rbPlayer.velocity = new Vector2(rbPlayer.velocity.x,JumpForce);
@@ -66,9 +77,20 @@ public class MovementPlayer : MonoBehaviour
     }
      void OnCollisionEnter2D(Collision2D collision)
     {
+
+        // resetear salto
         if (collision.collider.CompareTag("Floor"))
         {
-            Jumps = 0;
+
+
+            // Aquí se utiliza un condicional para verificar el ángulo entre la normal de la colisión y el vector hacia arriba (Vector2.up).
+            // La normal es un vector perpendicular a la superficie de colisión. 
+            // Este condicional verifica si el ángulo entre la normal y el vector hacia arriba es menor a 45 grados.
+            // conseguido de stack overflow de usuario "Voidsay"
+            if (Vector2.Angle(collision.GetContact(0).normal, Vector2.up) < 45)
+            {
+                Jumps = 0;
+            }
 
         }
      
